4a599a178dd8ba013eaedf5a6dc56330da26e2cb
MatGod7 <49246264+MatGod7@users.noreply.github.com>
2023-09-29 21:00:55 -0300

    Update main.py
    probando de nuevo pero con cambio en la región (us central [14])

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CD nuevo'
+    return 'Hello World! CD nuevo ahora si que si !'
 
 @app.route('/echo/<name>')
 def echo(name):
